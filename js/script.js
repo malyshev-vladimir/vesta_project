9caf4f978bfbccf13180d5da480cb79635993444
Malyshev Vova <malyshevvova@MBP-Malyshev.Dlink>
2020-07-26 13:40:40 +0300

    add some changes

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('heart'),
+      oneHeart = document.querySelector('.heart');
+
+console.dir(box);
+
+box.style.backgroundColor = 'blue';
+box.style.width = '500px';
+
+btns[1].style.borderRadius = '100%';
+
+circles[0].style.backgroundColor = 'red';
